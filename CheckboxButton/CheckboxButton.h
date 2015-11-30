c1f09d6c55309d5c3b045d06353fed2b5ec84c36
Chris Amanse <christopheramanse@gmail.com>
2015-11-30 10:20:24 +0800

    Initial Commit

---
+++ CheckboxButton/CheckboxButton.h
@@ -0,0 +1,19 @@
+//
+//  CheckboxButton.h
+//  CheckboxButton
+//
+//  Created by Joe Amanse on 30/11/2015.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CheckboxButton.
+FOUNDATION_EXPORT double CheckboxButtonVersionNumber;
+
+//! Project version string for CheckboxButton.
+FOUNDATION_EXPORT const unsigned char CheckboxButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckboxButton/PublicHeader.h>
+
+
