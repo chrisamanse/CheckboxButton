bfd627a7474c94bb29ac8ab3b8204e1a3a01d13e
Chris Amanse <christopheramanse@gmail.com>
2015-11-30 11:31:52 +0800

    Add checkbox button class and example app

---
+++ CheckboxButton-exampleTests/CheckboxButton_exampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckboxButton_exampleTests.swift
+//  CheckboxButton-exampleTests
+//
+//  Created by Joe Amanse on 30/11/2015.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import XCTest
+@testable import CheckboxButton_example
+
+class CheckboxButton_exampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ CheckboxButton/CheckboxButton.swift
@@ -0,0 +1,134 @@
+//
+//  CheckboxButton.swift
+//  CheckboxButton
+//
+//  Created by Joe Amanse on 30/11/2015.
+//  Copyright © 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class CheckboxButton: UIButton {
+    // MARK: Box and check properties
+    let boxLayer = CAShapeLayer()
+    let checkLayer = CAShapeLayer()
+    
+    var boxFrame: CGRect {
+        let width = bounds.width
+        let height = bounds.height
+        
+        let x: CGFloat
+        let y: CGFloat
+        
+        let sideLength: CGFloat
+        if width > height {
+            sideLength = height
+            x = (width - sideLength) / 2
+            y = 0
+        } else {
+            sideLength = width
+            x = 0
+            y = (height - sideLength) / 2
+        }
+        
+        let halfLineWidth = boxLineWidth / 2
+        return CGRect(x: x + halfLineWidth, y: y + halfLineWidth, width: sideLength - boxLineWidth, height: sideLength - boxLineWidth)
+    }
+    
+    var boxPath: UIBezierPath {
+        return UIBezierPath(rect: boxFrame)
+    }
+    var checkPath: UIBezierPath {
+        let inset = (boxLineWidth + checkLineWidth) / 2
+        let innerRect = CGRectInset(boxFrame, inset, inset)
+        let path = UIBezierPath()
+        
+        let unit = innerRect.width / 8
+        let origin = innerRect.origin
+        let x = origin.x
+        let y = origin.y
+        
+        path.moveToPoint(CGPoint(x: x + unit, y: y + unit * 5))
+        path.addLineToPoint(CGPoint(x: x + unit * 3, y: y + unit * 7))
+        path.addLineToPoint(CGPoint(x: x + unit * 7, y: y + unit))
+        
+        return path
+    }
+    
+    // MARK: Inspectable properties
+    @IBInspectable public var boxLineWidth: CGFloat = 2.0 {
+        didSet {
+            layoutLayers()
+        }
+    }
+    @IBInspectable public var checkLineWidth: CGFloat = 2.0 {
+        didSet {
+            layoutLayers()
+        }
+    }
+    @IBInspectable public var boxLineColor: UIColor = UIColor.blackColor() {
+        didSet {
+            colorLayers()
+        }
+    }
+    @IBInspectable public var checkLineColor: UIColor = UIColor.blackColor() {
+        didSet {
+            colorLayers()
+        }
+    }
+    
+    // MARK: Initialization
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override func awakeFromNib() {
+        customInitialization()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInitialization()
+    }
+    
+    func customInitialization() {
+        boxLayer.fillColor = UIColor.clearColor().CGColor
+        checkLayer.fillColor = UIColor.clearColor().CGColor
+        
+        colorLayers()
+        layoutLayers()
+        
+        layer.addSublayer(boxLayer)
+        layer.addSublayer(checkLayer)
+    }
+    
+    // MARK: Layout layers
+    private func layoutLayers() {
+        boxLayer.frame = bounds
+        boxLayer.lineWidth = boxLineWidth
+        boxLayer.path = boxPath.CGPath
+        
+        checkLayer.frame = bounds
+        checkLayer.lineWidth = checkLineWidth
+        checkLayer.path = checkPath.CGPath
+    }
+    
+    // MARK: Color layers
+    private func colorLayers() {
+        boxLayer.strokeColor = boxLineColor.CGColor
+        checkLayer.strokeColor = selected ? checkLineColor.CGColor : UIColor.clearColor().CGColor
+    }
+    
+    // MARK: Selection
+    public override var selected: Bool {
+        didSet {
+            colorLayers()
+        }
+    }
+    
+    // MARK: Interface builder
+    public override func prepareForInterfaceBuilder() {
+        customInitialization()
+    }
+}
